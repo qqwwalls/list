10dcbda7349377273f57684a911ca40503578bd0
Olena Shcherbyna <lentosik75@gmail.com>
2025-10-06 17:52:04 +0300

    add tasks function describe

--- list/FileName.cpp
+++ list/FileName.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
 #include"func.h"
 using namespace std;
-struct Task {
-    char title[50];       // Назва справи
-    int priority;         // Пріоритет (1 - низький, 2 - середній, 3 - високий)
-    char description[100];// Опис
-    char deadline[20];    // Дата і час 
-};
 int main() {
+    Task tasks[100];
+    int count = 0;
 
+    add_task(tasks, &count);
+    show_task(tasks, &count);
 	return 0;
 }
--- list/func.h
+++ list/func.h
@@ -1,41 +1,68 @@
 #pragma once
-// Прототипи функцій
-
+const int MAX_TASKS = 100;
+struct Task {
+    char title[50];
+    int priority;         // Пріоритет (1 - низький, 2 - середній, 3 - високий)
+    char description[100];
+    char deadline[20];    // Дата, час 
+};
+/********************/
 // Додавання справи
-void add_task(Task tasks[], int& count);
+void add_task(Task tasks[], int* count) {
+    if (*count >= MAX_TASKS) {                
+        cout << "List overflow!\n";
+        return;
+    }
+
+    cout << "Enter name: ";
+    cin.ignore();                               
+    cin.getline(tasks[*count].title, 50);     
+
+    cout << "Enter priority (1-low,2-middle,3-high): ";
+    cin >> tasks[*count].priority;
+    cin.ignore();
+
+    cout << "Enter description: ";
+    cin.getline(tasks[*count].description, 100);
+
+    cout << "Enter date and time: ";
+    cin.getline(tasks[*count].deadline, 20);
+
+    (*count)++;                              
+    cout << "List done!\n";
+}
+// Відображення всіх справ
+void show_task(Task tasks[], int* count);
 
 // Видалення справи
-void remove_task(Task tasks[], int& count, int index);
+void remove_task(Task tasks[], int* count, int index);
 
 // Редагування справи
-void editTask(Task tasks[], int count, int index);
-
-// Відображення всіх справ
-void showTasks(Task tasks[], int count);
+void edit_task(Task tasks[], int *count, int index);
 
 // Пошук за назвою
-void searchByTitle(Task tasks[], int count, const char title[]);
+void search_by_title(Task tasks[], int *count, const char title[]);
 
 // Пошук за пріоритетом
-void searchByPriority(Task tasks[], int count, int priority);
+void search_by_priority(Task tasks[], int *count, int priority);
 
 // Пошук за описом
-void searchByDescription(Task tasks[], int count, const char desc[]);
+void search_by_description(Task tasks[], int *count, const char desc[]);
 
 // Пошук за датою
-void searchByDate(Task tasks[], int count, const char deadline[]);
+void search_by_date (Task tasks[], int *count, const char deadline[]);
 
 // Відображення завдань на день
-void showTasksForDay(Task tasks[], int count, const char date[]);
+void show_tasks_for_day(Task tasks[], int *count, const char date[]);
 
 // Відображення завдань на тиждень
-void showTasksForWeek(Task tasks[], int count, const char startDate[], const char endDate[]);
+void show_tasks_for_week(Task tasks[], int *count, const char startDate[], const char endDate[]);
 
 // Відображення завдань на місяць
-void showTasksForMonth(Task tasks[], int count, const char month[]);
+void show_tasks_for_month(Task tasks[], int *count, const char month[]);
 
 // Сортування за пріоритетом
-void sortByPriority(Task tasks[], int count);
+void sort_by_priority(Task tasks[], int *count);
 
 // Сортування за датою
-void sortByDeadline(Task tasks[], int count);
+void sort_by_deadline (Task tasks[], int *count);
